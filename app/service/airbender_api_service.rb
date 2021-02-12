c79c2c3548e02190d4092118e8ad56d17fa92550
Max Ribbans <ribbans.max@gmail.com>
2021-02-12 12:42:10 -0500

    Fully refactor req.params

--- app/service/airbender_api_service.rb
+++ app/service/airbender_api_service.rb
@@ -1,13 +1,17 @@
 class AirbenderApiService
   class << self
     def number(nation)
-      response = Faraday.new("https://last-airbender-api.herokuapp.com/api/v1/characters/?affiliation=Fire+Nation&perPage=200").get
+      response = Faraday.new("https://last-airbender-api.herokuapp.com/api/v1/characters/").get do |req|
+        req.params['perPage'] = "200"
+        req.params['affiliation'] = nation_lookup(nation)
+      end
       parse(response).count
     end
 
     def members(nation)
-      response = Faraday.new("https://last-airbender-api.herokuapp.com/api/v1/characters/?affiliation=Fire+Nation").get do |req|
+      response = Faraday.new("https://last-airbender-api.herokuapp.com/api/v1/characters/").get do |req|
         req.params['perPage'] = "25"
+        req.params['affiliation'] = nation_lookup(nation)
       end
       parse(response)
     end
@@ -19,7 +23,7 @@ class AirbenderApiService
 
     def nation_lookup(nation)
       table = { 
-        "fire_nation" => "Fire+Nation"
+        "fire_nation" => "Fire Nation"
       }
       table[nation]
     end
