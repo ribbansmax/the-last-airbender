0fd4227e8591b6b3d1459aeeffeeefb81f80645a
Max Ribbans <ribbans.max@gmail.com>
2021-02-12 11:32:32 -0500

    Add working search select

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'When I visit welcome page' do
-  xit 'should navigate to the search page' do
+  it 'should navigate to the search page' do
     visit root_path
     
-    select "fire_nation", :from => "nation"
+    select "Fire Nation", :from => "nation"
 
     click_button 'Search For Members'
+
+    expect(current_path).to eq(search_path)
   end
 end
